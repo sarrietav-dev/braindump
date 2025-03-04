331d79c0a858a118bab46e911d85ff1ae5885d2d
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-03-04 17:47:55 -0500

    Refactor Head component to simplify Google Fonts integration by removing conditional rendering

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -158,13 +158,9 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
-          <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
-          </>
-        )}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin={"anonymous"} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* OG/Twitter meta tags */}
